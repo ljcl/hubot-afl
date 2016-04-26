b0ad58751129c5b94c97b5f2ade16d3ceeab2fe0
Luke Clark <contact@lukejclark.com>
2016-04-26 14:22:13 +1000

    Better token checking, fix specify year, token debugging

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ var aflToken
 function getToken (cb) {
   if (typeof tokenRetrieved !== 'undefined') {
     // There is a token, check date
-    if (tokenRetrieved.isAfter(tokenRefresh)) {
+    if (tokenRetrieved.isSameOrAfter(tokenRefresh, 'day')) {
       // Get a new token
       newToken(function (err, token) {
         if (err) throw new Error(err)
@@ -33,21 +33,13 @@ function getToken (cb) {
 
 function newToken (cb) {
   tokenRetrieved = moment()
-  tokenRefresh = tokenRetrieved.add(1, 'days')
+  tokenRefresh = moment().add(1, 'days')
   request({method: 'POST', url: 'http://api.afl.com.au/cfs/afl/WMCTok'}, function (error, response, body) {
     if (error) throw new Error(error)
     var data = JSON.parse(body)
     cb(false, data.token)
   })
 }
-// We need a token to use the AFL API.
-// if (typeof process.env.AFLTOKEN === 'undefined') {
-//   request({method: 'POST', url: 'http://api.afl.com.au/cfs/afl/WMCTok'}, function (error, response, body) {
-//     if (error) throw new Error(error)
-//     var data = JSON.parse(body)
-//     process.env.AFLTOKEN = data.token
-//   })
-// }
 
 function formatMatch (item, cb) {
   var match
@@ -96,7 +88,10 @@ function getId (match, round, year, cb) {
   } else {
     match = ''
   }
+  // If it's not a year or a year in the future, set it to the current year.
+  // TODO: Allow further in the future if we can assume the schedule is released.
   year = (!moment(year, 'YYYY', true).isValid() ? moment().year() : year)
+  year = (moment(year, 'YYYY', true).isAfter(moment()) ? moment().year() : year)
   result.year = year
   result.id = 'CD_' + prefix + year + '014' + leftpad(round, 2, 0) + match
   // e.g: CD_R201501401
@@ -104,13 +99,12 @@ function getId (match, round, year, cb) {
 }
 
 module.exports = (robot) => {
-  robot.hear(/(show me)? (afl round) (.*)( .*)?/i, (res) => {
+  robot.hear(/(show me)?\s?(afl round)\s([0-9]{1,2})\s?([0-9]{4})?/i, (res) => {
     var round = res.match[3].replace(/\?/g, '')
     var year = (typeof res.match[4] !== 'undefined' ? res.match[4] : '')
     getId(false, round, year, function (round) {
       getToken(function (err, token) {
         if (err) throw new Error(err)
-        console.log('yay', token)
         var optionsRound = {
           url: 'http://api.afl.com.au/cfs/afl/matchItems/round/' + round.id,
           headers: {'x-media-mis-token': aflToken}
@@ -131,18 +125,27 @@ module.exports = (robot) => {
               if (count === total) {
                 res.send(message)
               } else {
-                 message += '\n'
+                message += '\n'
               }
             })
           } else if (response.statusCode === 404) {
             res.send('You probably did something wrong (' + response.statusCode + ' ' + response.statusMessage + ')')
           } else {
-            res.send('We probably did something wrong (' + response.statusCode + ' ' + response.statusMessage + ')')
-            console.warn('This token may not be valid: ', aflToken)
+            res.send('We probably did something wrong (' + response.statusCode + ')')
             console.warn(body)
           }
         })
       })
+    })
+  })
+  // Check our afl token and it's age (debug)
+  robot.hear(/(afl token)/i, (res) => {
+    getToken(function (err, token) {
+      if (err) throw new Error(err)
+      var debugString = 'AFL API Token ' + aflToken + ': \n'
+      debugString += 'Retrieved: ' + tokenRetrieved.format('dddd, MMMM Do YYYY, h:mm:ss a') + '\n'
+      debugString += 'Refreshes: ' + tokenRefresh.format('dddd, MMMM Do YYYY, h:mm:ss a')
+      res.send(debugString)
     })
   })
 }
