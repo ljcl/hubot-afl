8fd559c7b19c9798c372c9f466e5603009ffdf8d
Luke Clark <contact@lukejclark.com>
2016-04-26 14:22:27 +1000

    More tests

--- test/index.js
+++ test/index.js
@@ -4,6 +4,8 @@ var Robot = require('hubot/src/robot')
 var TextMessage = require('hubot/src/message').TextMessage
 var expect = require('chai').expect
 
+var moment = require('moment-timezone')
+
 describe('hubot', function () {
   var robot
   var user
@@ -31,13 +33,46 @@ describe('hubot', function () {
     robot.shutdown()
   })
 
-  it('should send a message when being asked for season information', (done) => {
+  it('Return information about an AFL season from the current year', (done) => {
+    robot.adapter.on('send', (envelope, strings) => {
+      expect(strings[0]).to.match(/Here\'s round/i)
+      done()
+    })
+    // Send a message to Hubot
+    robot.adapter.receive(new TextMessage(user, 'show me afl round 5'))
+  })
+
+  it('Return information about an AFL season from last year', (done) => {
+    var lastYear = moment().subtract(1, 'years').format('Y')
+
     robot.adapter.on('send', (envelope, strings) => {
       expect(strings[0]).to.match(/Here\'s round/i)
       done()
     })
 
     // Send a message to Hubot
-    robot.adapter.receive(new TextMessage(user, 'show me afl round 5'))
+    robot.adapter.receive(new TextMessage(user, 'show me afl round 5 ' + lastYear))
+  })
+
+  it('Return the current year if AFL season query is too far in the future', (done) => {
+    var nextYear = moment().add(1, 'years').format('Y')
+
+    robot.adapter.on('send', (envelope, strings) => {
+      expect(strings[0]).to.match(/Here\'s round/i)
+      done()
+    })
+
+    // Send a message to Hubot
+    robot.adapter.receive(new TextMessage(user, 'show me afl round 1' + nextYear))
+  })
+
+  it('Retrieve information about the api token', (done) => {
+    robot.adapter.on('send', (envelope, strings) => {
+      expect(strings[0]).to.match(/AFL API Token/i)
+      done()
+    })
+
+    // Send a message to Hubot
+    robot.adapter.receive(new TextMessage(user, 'afl token'))
   })
 })
